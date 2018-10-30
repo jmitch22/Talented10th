a6489e970a908910f72d77dbc97241b5c52c0e97
jmitch22 <30491720+jmitch22@users.noreply.github.com>
2018-10-30 18:35:23 -0400

    Create Physique.java

---
+++ Physique.java
@@ -0,0 +1,92 @@
+/*Physique Class(JADA)
+Weight
+Age
+Height
+Prefered age min
+Prefered age max
+Prefered weight min
+Prefered weight max
+Prefered height min
+Prefered height max
+
+*/
+public class Physique{
+
+
+    private int weight ;
+    private int age;
+    private int height;
+    private int preferedAgeMin;
+    private int preferedAgeMax;
+    private int preferedWeightMin;
+    private int preferedWeightmax;
+    private int preferedHeighttMin;
+    private int preferedHeightMax;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age){
+      this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+   
+    public int getPreferedAgeMin() {
+        return preferedAgeMin;
+    }
+
+    public void setPreferedAgeMin(int age) {
+        this.preferedAgeMin = preferedAgeMin;
+    }
+     public int getPreferedAgeMax() {
+        return preferedAgeMax;
+    }
+
+    public void setPreferedAgeMax(int age) {
+        this.preferedAgeMax = preferedAgeMax;
+    }
+
+    public int getPreferedWeightMin() {
+        return preferedWeightMin;
+    }
+
+    public void setPreferedWeightMin(int age) {
+        this.preferedWeightMin = preferedWeightMin;
+    }
+     public int getPreferedWeightMax() {
+        return preferedWeightMax;
+    }
+
+    public void setPreferedWeightMax(int age) {
+        this.preferedWeightMax = preferedWeightMax;
+    }
+     public int getPreferedHeightMin() {
+        return preferedHeightMin;
+    }
+
+    public void setPreferedHeightMin(int age) {
+        this.preferedHeightMin = preferedHeightMin;
+    }
+     public int getPreferedHeightMax() {
+        return preferedHeightMax;
+    }
+
+    public void setPreferedHeightMax(int age) {
+        this.preferedHeightMax = preferedHeightMax;
+    }
+}
